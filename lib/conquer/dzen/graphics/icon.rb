e90637dec4bad284d8be99b3a5f91be6615f84b6
Joakim Reinert <mail@jreinert.com>
2016-06-05 22:13:47 +0200

    add icons to gem for easy usage using sybmols

--- lib/conquer/dzen/graphics/icon.rb
+++ lib/conquer/dzen/graphics/icon.rb
@@ -4,10 +4,13 @@ module Conquer
   module Dzen
     module Graphics
       class Icon < Graphic
+        ICONS_PATH = File.expand_path('../../../../../icons', __FILE__)
         attr_reader :path
 
         def initialize(path)
           super()
+          path = File.join(ICONS_PATH, "#{path}.xbm") if path.is_a?(Symbol)
+          STDERR.puts(path)
           @path = path
           @template = '^i(%s)'
           @template_args = [path]
